8e219093c91a4eade84536ab19d7a75f52ffab78
Tio Jobs <icarojobsoficial@gmail.com>
2023-10-03 13:41:34 -0300

    style: typo in smoke test

--- tests/architecture/test_architecture.py
+++ tests/architecture/test_architecture.py
@@ -2,5 +2,5 @@ from decouple import config
 
 
 def test_smoke_test():
-    DB_NAME = config('POSTGRES_DB')
-    assert config('DB_URL') == f"postgresql+psycopg2://database/{DB_NAME}?user=admin&password=password"
+    database_name = config('POSTGRES_DB')
+    assert config('DB_URL') == f"postgresql+psycopg2://database/{database_name}?user=admin&password=password"
