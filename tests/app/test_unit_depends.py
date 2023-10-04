8a6a6a974b617337bcb1cfafa359e10bb16168ac
Tio Jobs <icarojobsoficial@gmail.com>
2023-10-04 19:05:47 -0300

    refactor: changed test_unit_depends class name

--- tests/app/test_unit_depends.py
+++ tests/app/test_unit_depends.py
@@ -10,7 +10,7 @@ from fastapi import Depends
 from app.depends import get_db_session
 
 
-class TestCalculations(unittest.TestCase):
+class TestUnitDepends(unittest.TestCase):
     def test_get_db_session_method_a_fastapi_depends(self):
         session = Depends(get_db_session)
         self.assertTrue(isinstance(session, type(fastapi.Depends())))
